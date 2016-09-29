2b6d02cb35ea5434a3edb4bc5b82af34159649d9
tsavery <savery.theron@gmail.com>
2016-09-28 20:51:41 -0400

    Added 4 player multiplayer, simple projetiles, respawn system

---
+++ Assets/Scripts/Player/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if(collision.gameObject.tag.Equals("Level"))
+			Destroy(gameObject);
+
+		if(collision.gameObject.layer == 8 ) {
+			collision.gameObject.SendMessage("OnHit");
+			Destroy(gameObject);
+		}
+	}
+
+	void OnBecameInvisible() {
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/Player/PlayerSpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSpawner : MonoBehaviour {
+
+
+	public GameObject playerObject;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	// Update is called once per frame	
+	void Update () {
+	
+	}
+
+	public void SpawnPlayer(int playerNumber) {
+		
+		GameObject spawnedPlayer = (GameObject)Instantiate(playerObject, transform.position, transform.rotation);
+		spawnedPlayer.GetComponent<Player>().playerNumber = playerNumber;
+		switch (playerNumber) {
+		case 1: spawnedPlayer.GetComponent<MeshRenderer>().material.color  = Color.blue;
+			break;
+		case 2: spawnedPlayer.GetComponent<MeshRenderer>().material.color = Color.green;
+			break;
+		case 3: spawnedPlayer.GetComponent<MeshRenderer>().material.color = Color.yellow;
+			break;
+		case 4: spawnedPlayer.GetComponent<MeshRenderer>().material.color = Color.cyan;
+			break;
+		}	
+
+	}
+}
